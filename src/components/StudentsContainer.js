6584217a23f0e24c1c4f82d3445d06af433c4c34
Lianne van Westrhenen <lianne.van.westrhenen@gmail.com>
2017-12-14 12:26:16 +0100

    show students in batch

--- src/components/StudentsContainer.js
+++ src/components/StudentsContainer.js
@@ -1,40 +1,64 @@
-//overview of all students in same batch. Renders StudentPage
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { fetch as fetchStudent } from '../actions/student/fetch'
-import StudentItem from './StudentItem'
+import {push} from 'react-router-redux'
+import { fetchOneBatch } from '../actions/batch/fetch'
+import { Link } from 'react-router-dom'
+import './StudentsContainer.css'
 
-
-class StudentContainer extends PureComponent {
-  componentWillMount() {
-    this.props.dispatch(fetchStudent())
+class Batch extends PureComponent {
+  static propTypes = {
+    fetchOneBatch: PropTypes.func,
+    batch: PropTypes.shape({
+      _id: PropTypes.string,
+      number: PropTypes.number,
+      students: PropTypes.arrayOf(PropTypes.shape({
+        _id: PropTypes.string,
+        firstName: PropTypes.string,
+        picture: PropTypes.string,
+        evaluation: PropTypes.arrayOf(PropTypes.shape({
+          colour: PropTypes.string,
+          date: PropTypes.date,
+          remark: PropTypes.string,
+        }))
+      }))
+    }),
   }
 
-  renderStudents(batch, index) {
-    return (
-      <StudentItem key={index} {...batch} />
-    )
+  componentWillMount() {
+    const { batch, fetchOneBatch } = this.props
+    const { batchId } = this.props.match.params
+
+    if (!batch) { fetchOneBatch(batchId) }
   }
 
   render() {
-    const { students } = this.props
+    const { batch } = this.props
 
-    if (!students) { return null }
+    if (!batch) return null
 
     return(
       <div>
-        <header>
-          <p>"All current batches"</p>
-        </header>
+        <h1>This is batch.. #{batch.number}</h1>
 
-        <main>
-          {students.map(this.renderStudents)}
-        </main>
+        {batch.students.map((student) =>
+          <li>{student.firstName} <br />
+              <img className="picture" src={student.picture} alt=""/>
+          </li>
+        )}
+
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ students }) => ({ students })
+// filter as in GameContainer
+const mapStateToProps = ({ batches }, { match }) => {
+  const batch = batches.filter((g) => (g._id === match.params.batchId))[0]
 
-export default connect(mapStateToProps)(StudentContainer)
+  return {
+    batch
+  }
+}
+
+export default connect(mapStateToProps, {fetchOneBatch, push})(Batch)
